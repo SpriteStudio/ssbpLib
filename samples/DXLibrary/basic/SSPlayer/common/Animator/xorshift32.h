08c24dc0cde3bbce263f7a96aeebece641538512
DKAlpha <dkredint@gmail.com>
2016-07-01 17:10:52 +0900

    Macでエフェクトが動作しない問題の修正

--- samples/DXLibrary/basic/SSPlayer/common/Animator/xorshift32.h
+++ samples/DXLibrary/basic/SSPlayer/common/Animator/xorshift32.h
@@ -2,19 +2,30 @@
 #ifndef __XORSHIFT32__
 #define __XORSHIFT32__
 
+#ifdef WIN32
 
+	#ifndef uint32_t	
+		typedef unsigned int _uint32_t;
+	#endif
+#else
+	typedef  uint32_t	_uint32_t;
+#endif
 
 class xorshift32
 {
 protected:
-    unsigned long y;
+
+
+	_uint32_t	y;
+
+
 public:
 	xorshift32(){}
 	~xorshift32(){}
 
 	void	init_genrand(unsigned long seed) {y=seed;}
 
-	unsigned long genrand_uint32(){
+	_uint32_t genrand_uint32(){
 		y = y ^ ( y << 13 );
 		y = y ^ ( y >> 17 );
 		y = y ^ ( y << 15 );
@@ -23,9 +34,11 @@ public:
 	}
 
 	float genrand_float32() {
-		unsigned long v = genrand_uint32();
-		unsigned res = (v >> 9) | 0x3f800000;
-		return (*(float*)&res) - 1.0f;
+		_uint32_t v = genrand_uint32();
+		_uint32_t res = (v >> 9) | 0x3f800000;
+		float	r = (*(float*)&res) - 1.0f;
+
+		return r;
 	}
 
 } ;
