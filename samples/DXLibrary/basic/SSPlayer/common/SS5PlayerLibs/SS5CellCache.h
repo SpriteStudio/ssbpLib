1a8e9ac36d35d0ef72daccdd80788d2960ee4321
kagematya <kagematya@gmail.com>
2016-11-03 14:51:03 +0900

    CellCacheを分離

---
+++ samples/DXLibrary/basic/SSPlayer/common/SS5PlayerLibs/SS5CellCache.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include "SS5PlayerTypes.h"
+#include "common/Loader/sstypes.h"
+
+
+namespace ss{
+class Cell;
+struct ProjectData;
+
+
+/**
+ * CellRef
+ */
+struct CellRef{
+	const Cell* cell;
+	TextuerData texture;
+	SSRect rect;
+	std::string texname;
+};
+
+
+/**
+ * CellCache
+ */
+class CellCache{
+public:
+	CellCache();
+	~CellCache();
+	
+	static CellCache* create(const ProjectData* data, const std::string& imageBaseDir);
+
+	CellRef* getReference(int index);
+
+	//指定した名前のセルの参照テクスチャを変更する
+	bool setCellRefTexture(const ProjectData* data, const char* cellName, long texture);
+
+
+	//指定したデータのテクスチャを破棄する
+	bool releseTexture(const ProjectData* data);
+
+private:
+	void init(const ProjectData* data, const std::string& imageBaseDir);
+
+	//キャッシュの削除
+	void releseReference(void);
+
+	void addTexture(const std::string& imagePath, const std::string& imageBaseDir, SsTexWrapMode::_enum  wrapmode, SsTexFilterMode::_enum filtermode);
+
+protected:
+	std::vector<std::string>			_texname;
+	std::vector<TextuerData>			_textures;
+	std::vector<CellRef*>				_refs;
+};
+
+
+
+
+} //namespace ss;
+
+
